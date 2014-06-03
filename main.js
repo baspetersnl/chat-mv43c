e630b8bb7a00539fbbf3f706aa9fa89a25292ddd
https://github.com/baspetersnl/MV43C-chat.git <Bas@MacBook-Pro-van-Bas.local>
2014-06-03 16:44:34 +0200

    adding files

---
+++ main.js
@@ -0,0 +1,36 @@
+var http = require('http'),
+	fs = require('fs'),
+	sanitize = require('validator').sanitize;
+	
+var app = http.createServer(function (request, response) {
+	fs.readFile("client.html", 'utf-8', function (error, data) {
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write(data);
+		response.end();
+	});
+}).listen(8000);
+
+var io = require('socket.io').listen(app);
+
+var people = {};
+
+io.sockets.on('connection', function(socket) {
+	
+	socket.on("join", function(name){
+		people[socket.id] = name;
+		io.sockets.emit("update", name + " is verbonden met de server.")
+		io.sockets.emit("update-people", people);
+	});
+
+	socket.on("send", function(msg){
+		io.sockets.emit("chat", people[socket.id], msg);
+	});
+
+	socket.on("disconnect", function(){
+		io.sockets.emit("update", people[socket.id] + " is heeft verbinding verbroken.");
+		delete people[socket.id];
+		io.sockets.emit("update-people", people);
+	});
+	
+	
+});
